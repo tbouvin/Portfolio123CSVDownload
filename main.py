7e0f32c1e6bf9f0e91c51e2ceeaa32896654392c
tbouvin <tbouvin@gmail.com>
2017-04-03 20:35:16 -0500

    Added functions to reduce code reuse

--- main.py
+++ main.py
@@ -1,52 +1,74 @@
-import sys, getopt
+import sys, getopt, time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-# import selenium.webdriver.common.keys
 
-# def getElement(driver, name):
-# 	elem = None
-# 	try:
-# 		elem = driver.find_element_by_id(name)
-# 		print(name + ' field found successfully')
-# 	except NoSuchElementException:
-# 		print(name + ' field not found')
-#
-# 	return elem
+def enterText(driver, elementId, text):
+	try:
+		driver.find_element_by_id(elementId).send_keys(text)
+	except NoSuchElementException:
+		print("No", elementId, "element")
+
+def clickButton(driver, elementId):
+	try:
+		driver.find_element_by_id(elementId).click()
+	except NoSuchElementException:
+		print("No", elementId, "element")
 
 def main(argv):
 	username = ''
 	password = ''
+
 	try:
 		opts, args = getopt.getopt(argv, "hu:p:", ["username=", "password="])
 	except getopt.GetoptError:
-		print ('test.py -i <inputfile> -o <outputfile>')
-		sys.exit(2)
+		print('Invalid option')
+		print('test.py -i <inputfile> -o <outputfile>')
+		exit()
+
 	for opt, arg in opts:
 		if opt == '-h':
-			print ('test.py -u <username> -p <password>')
-			sys.exit()
+			print('test.py -u <username> -p <password>')
+			exit()
 		elif opt in ("-u", "--username"):
 			username = arg
 		elif opt in ("-p", "--password"):
 			password = arg
-	# print ('Username is "' + username + "\"")
-	# print ('Password is "' + password + "\"")
 
 	if len(username) == 0:
-		print ('Username not entered')
+		print('Username not entered')
 		print('test.py -u <username> -p <password>')
-		sys.exit()
+		exit()
+
 	if len(password) == 0:
 		print('Password not entered')
 		print('test.py -u <username> -p <password>')
-		sys.exit()
+		exit()
 
-	driver = webdriver.Chrome()
+	# if len(csv) == 0:
+	# 	print('CSV not entered')
+	# 	print('test.py -u <username> -p <password> -c <csv>')
+	# 	exit()
+
+#Try using chrome webdriver first. If that fails use Firefox
+	driver = None
+	try:
+		driver = webdriver.Chrome()
+	except FileNotFoundError:
+		print('Chrome web driver not found')
+		print('Trying Firefox')
+		try:
+			driver = webdriver.Firefox()
+		except FileNotFoundError:
+			print('Firefox web driver not found')
+			exit()
+
+#Start selenium and go to login jpage
 	driver.get("https://www.portfolio123.com/login.jsp?url=/")
 
-	driver.find_element_by_id('LoginUsername').send_keys(username)
-	driver.find_element_by_id('LoginPassword').send_keys(password)
+#Enter credentials and login
+	enterText(driver, "LoginUsername", username)
+	enterText(driver, "LoginPassword", password)
+	clickButton(driver, "Login")
 
 
 
